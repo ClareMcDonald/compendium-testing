972e2425dfe904b6066bf94b09bede2375391b07
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-04-26 17:48:03 -0700

    search works and all pokemon are displayed

--- src/components/PokeList/PokeList.jsx
+++ src/components/PokeList/PokeList.jsx
@@ -2,12 +2,23 @@ import { useEffect, useState } from 'react';
 
 export default function PokeList() {
     const [pokemon, setPokemon] = useState([]);
+    const [searchedPokemon, setSearchedPokemon] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [search, setSearch] = useState('');
+
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (search) {
+            const filteredPokemon = pokemon.filter(pokemon => pokemon.name.includes(search));
+
+            setSearchedPokemon(filteredPokemon);
+        }
+    }
 
     useEffect(() => {
         async function getPokemon() {
-            const res = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex');
-            // console.log(await res.json());
+            const res = await fetch('https://pokedex-alchemy.herokuapp.com/api/pokedex?perPage=801');
             const data = await res.json();
             const pokemonData = data.results;
             const pokemon = pokemonData.map((poke) => ({
@@ -31,21 +42,33 @@ export default function PokeList() {
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Name
-                    <input type='text'></input>  
+                    <input type='text' value={search} onChange={e => setSearch(e.target.value)}></input>  
                     <button>Search</button>
                 </label>
             </form>
-                {pokemon.map((poke, i ) => {
+            {search
+                ? searchedPokemon.map((poke, i) => {
                     return (
                     <div>
-                            <h2>{poke.name}</h2>
-                            <p>{poke.type}</p>
+                        <h2>{poke.name}</h2>
+                        <p>{poke.type}</p>
                         <img src={poke.img} alt='pic of pokemon' />
-                        </div>
+                    </div>
                     )
-                })}
+                })
+               : pokemon.map((poke, i) => {
+                return (
+                <div>
+                    <h2>{poke.name}</h2>
+                    <p>{poke.type}</p>
+                    <img src={poke.img} alt='pic of pokemon' />
+                </div>
+                )
+            })
+                
+            }
         </>
     )
 }
